cde1dcea678ff0cb9c89ca6e2243946b507d5a3b
Srijeetroy03 <srijeetroy03@gmail.com>
2023-03-16 02:37:14 +0530

    c

---
+++ js/contact.js
@@ -0,0 +1,26 @@
+let tablinks=document.getElementsByClassName("me");
+let tabcontents=document.getElementsByClassName("tab_contents");
+let navlinks=document.getElementsByClassName('nav')
+
+
+for(let navlink of navlinks){
+    navlink.addEventListener('mouseover',function(){
+        navlink.classList.add('active_link')
+    })
+    navlink.addEventListener('mouseout',function(){
+        navlink.classList.remove('active_link')
+    })
+
+}
+
+function opentab(tabname){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active_link")
+    }
+    
+
+    event.currentTarget.classList.add('active_link')
+
+}
+
+
