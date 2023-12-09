65f7ad9bbf0a3de01b468799b284dd1728fe4c89
Srijeetroy03 <srijeetroy03@gmail.com>
2023-12-10 01:23:22 +0530

    c

--- js/contact.js
+++ js/contact.js
@@ -1,28 +1,28 @@
-let tablinks=document.getElementsByClassName("me");
-let tabcontents=document.getElementsByClassName("tab_contents");
-let navlinks=document.getElementsByClassName('nav')
+// let tablinks=document.getElementsByClassName("me");
+// let tabcontents=document.getElementsByClassName("tab_contents");
+// let navlinks=document.getElementsByClassName('nav')
 
-function clicked(){
-    location.href="#"
-}
-for(let navlink of navlinks){
-    navlink.addEventListener('mouseover',function(){
-        navlink.classList.add('active_link')
-    })
-    navlink.addEventListener('mouseout',function(){
-        navlink.classList.remove('active_link')
-    })
+// function clicked(){
+//     location.href="#"
+// }
+// for(let navlink of navlinks){
+//     navlink.addEventListener('mouseover',function(){
+//         navlink.classList.add('active_link')
+//     })
+//     navlink.addEventListener('mouseout',function(){
+//         navlink.classList.remove('active_link')
+//     })
 
-}
+// }
 
-function opentab(tabname){
-    for(let tablink of tablinks){
-        tablink.classList.remove("active_link")
-    }
+// function opentab(tabname){
+//     for(let tablink of tablinks){
+//         tablink.classList.remove("active_link")
+//     }
     
 
-    event.currentTarget.classList.add('active_link')
+//     event.currentTarget.classList.add('active_link')
 
-}
+// }
 
 
--- js/index.js
+++ js/index.js
@@ -3,42 +3,54 @@ let tabcontents=document.getElementsByClassName("tab_contents");
 let navlinks=document.getElementsByClassName('nav')
 
 
-// function opennav(tabname){
-//     for(let navlink of navlinks){
-//         navlink.classList.remove("active_link")
-//     }
-//     event.currentTarget.classList.add('active_link')
-
-
-//     // document.getElementById(tabname).classList.add("active_tab")
-
-// }
 
 for(let navlink of navlinks){
     navlink.addEventListener('mouseover',function(){
-        navlink.classList.add('active_link')
+        // navlink.classList.add('active_link')
+        navlink.setAttribute('class','active_link')
     })
     navlink.addEventListener('mouseout',function(){
-        navlink.classList.remove('active_link')
+        // navlink.classList.remove('active_link')
+        navlink.removeAttribute('class','active_link')
     })
 
 }
 
-function opentab(tabname){
-    for(let tablink of tablinks){
-        tablink.classList.remove("active_link")
-    }
-    for(let tabcontent of tabcontents){
-        tabcontent.classList.remove("active_tab")
+let s=document.getElementById('skills')
+let exp=document.getElementById('experience')
+let edu=document.getElementById('education')
+let s_content=document.getElementById('skills_content')
+let exp_content=document.getElementById('experience_content')
+let edu_content=document.getElementById('education_content')
 
-    }
+s.addEventListener('click',()=>{
+    s.classList.add('active_link')
+    exp.classList.remove('active_link')
+    edu.classList.remove('active_link')
+    s_content.style.display="block"
+    exp_content.style.display="none"
+    edu_content.style.display="none"
     
+})
 
-    event.currentTarget.classList.add('active_link')
+exp.addEventListener('click',()=>{
+    s.classList.remove('active_link')
+    exp.classList.add('active_link')
+    edu.classList.remove('active_link')
+    s_content.style.display="none"
+    exp_content.style.display="block"
+    edu_content.style.display="none"
+})
+
+edu.addEventListener('click',()=>{
+    s.classList.remove('active_link')
+    exp.classList.remove('active_link')
+    edu.classList.add('active_link')
+    s_content.style.display="none"
+    exp_content.style.display="none"
+    edu_content.style.display="block"
+})
 
 
-    document.getElementById(tabname).classList.add("active_tab")
-
-}
 
 
